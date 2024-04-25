80e47301f75647c3d8b675d204eacf7ea7f59091
Mark Nebesniy <mark@nebesniy.com>
2024-04-25 06:18:50 +0200

    Create checker.php

---
+++ checker.php
@@ -0,0 +1,34 @@
+<?php
+
+// Проверка и установка настроек для отображения ошибок
+// Checking and setting settings for displaying errors
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
+
+// Проверка наличия и получение параметров url и word из GET-запроса
+// Checking the presence and obtaining the url and word parameters from a GET request
+$url = isset($_GET["url"]) ? urldecode($_GET["url"]) : null;
+$word = isset($_GET["word"]) ? urldecode($_GET["word"]) : null;
+
+// Проверка существования и валидности URL
+// Checking the existence and validity of a URL
+if (!$url || filter_var($url, FILTER_VALIDATE_URL) === false) {
+    die("URL не валиден");
+}
+
+// Получение содержимого URL с помощью cURL
+// Getting the content of a URL using cURL
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$content = curl_exec($curl);
+curl_close($curl);
+
+// Проверка наличия слова в содержимом
+// Checking for a word in the content
+if (strpos($content, $word) !== false) {
+    echo "1";
+} else {
+    echo "0";
+}
