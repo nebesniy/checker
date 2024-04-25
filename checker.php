c15fa454b12948ba820e0500a662ff827d7632f7
Mark Nebesniy <mark@nebesniy.com>
2024-04-25 06:33:10 +0200

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,32 +1,39 @@
 <?php
 
 // Проверка и установка настроек для отображения ошибок
-// Checking and setting settings for displaying errors
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 error_reporting(0);
 
 // Проверка наличия и получение параметров url и word из GET-запроса
-// Checking the presence and obtaining the url and word parameters from a GET request
 $url = isset($_GET["url"]) ? urldecode($_GET["url"]) : null;
 $word = isset($_GET["word"]) ? urldecode($_GET["word"]) : null;
 
 // Проверка существования и валидности URL
-// Checking the existence and validity of a URL
 if (!$url || filter_var($url, FILTER_VALIDATE_URL) === false) {
-    exit("URL не валиден / Invalid URL");
+    die("Not a valid URL");
 }
 
-// Получение содержимого URL с помощью cURL
-// Getting the content of a URL using cURL
+// Создание экземпляра cURL
 $curl = curl_init();
+
+// Установка URL и других параметров
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'nebesniy/checker'); // Установка пользовательского агента
+
+// Получение содержимого URL с помощью cURL
 $content = curl_exec($curl);
+
+// Проверка на ошибки
+if ($content === false) {
+    die("Error fetching URL: " . curl_error($curl));
+}
+
+// Закрытие cURL-соединения
 curl_close($curl);
 
 // Проверка наличия слова в содержимом
-// Checking for a word in the content
 if (strpos($content, $word) !== false) {
     echo "1";
 } else {
